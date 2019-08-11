9705d86d719e4c1171cb9c423a9af593b85bf1da
Bhaikko <siddhartha.pawar17@gmail.com>
2019-08-11 22:59:20 +0530

    Vendor dashboard Added

---
+++ Site/private/vendor/index.js
@@ -0,0 +1,39 @@
+let genres = $("#genres");
+let components = $("#components");
+let accessories = $("#accessories");
+let companies = $("#companies");
+
+let gamesButton = $("#gamesButton");
+let componentsButton = $("#componentsButton");
+let accessoriesButton = $("#accessoriesButton");
+let consolesButton = $("#consolesButton");
+
+let filters = $(".filter");
+let filterButton = $("#filterButton");
+
+const disableFilters = () => {
+    filters.each((index, filter) => {
+        filter.setAttribute("hidden", "");
+    })
+}
+disableFilters();
+
+gamesButton.click(() => {
+    disableFilters();
+    genres[0].removeAttribute("hidden");
+});
+
+componentsButton.click(() => {
+    disableFilters();
+    components[0].removeAttribute("hidden");
+});
+
+accessoriesButton.click(() => {
+    disableFilters();
+    accessories[0].removeAttribute("hidden");
+});
+
+consolesButton.click(() => {
+    disableFilters();
+    companies[0].removeAttribute("hidden");
+});
