5e078324c29a579314b1e14e432540b92a126211
Bhaikko <siddhartha.pawar17@gmail.com>
2019-08-14 22:58:12 +0530

    Search Bar Added on myproducts

--- Site/private/vendor/myproducts.js
+++ Site/private/vendor/myproducts.js
@@ -16,11 +16,10 @@ const render = (box, products) => {
         box.append(
             `
             <div class="product bg-light text-dark p-3 mb-2">
-                <div class="productid" hidden>${product.id}</div>
                 <div class="productName">Name: <b>${product.name}</b></div>
                 <div class="productPrice">Price: <b>${product.price}</b></div>
                 <div class="productSubtype">SubType: <b>${product.productSubtype}</b></div>
-                <button type="button" class="btn btn-danger deleteProductButton"  style="margin-bottom: 10px;">Delete Product</button>
+                <button type="button" class="btn btn-danger deleteProductButton"  style="margin-bottom: 10px;" value="${product.id}">Delete Product</button>
             </div>
             `
         );
@@ -53,7 +52,7 @@ $.get("/vendor/products", (products) => {
                 url: '/vendor/deleteProduct',
                 type: 'DELETE',
                 data: {
-                    id: event.target.parentNode.children[0].innerText
+                    id: event.target.value
                 },
                 success: function() {
                     event.target.parentNode.remove();
@@ -63,5 +62,46 @@ $.get("/vendor/products", (products) => {
     })
     
 });
+
+let gamesSearch = $("#gamesSearch");
+gamesSearch[0].addEventListener("input", () => {
+    const filtered = games.filter(game => {
+        if(game.name.toLowerCase().includes(gamesSearch[0].value.toLowerCase()))
+            return game;
+    });
+    render(gamesBox, filtered); 
+});
+
+let consolesSearch = $("#consolesSearch");
+consolesSearch[0].addEventListener("input", () => {
+    const filtered = consoles.filter(consol => {
+        if(consol.name.toLowerCase().includes(consolesSearch[0].value.toLowerCase()))
+            return consol;
+    });
+    render(consolesBox, filtered); 
+});
+
+
+let accessoriesSearch = $("#accessoriesSearch");
+accessoriesSearch[0].addEventListener("input", () => {
+    const filtered = accessories.filter(accessory => {
+        if(accessory.name.toLowerCase().includes(accessoriesSearch[0].value.toLowerCase()))
+            return accessory;
+    });
+    render(accessoriesBox, filtered); 
+});
+
+let componentSearch = $("#componentSearch");
+componentSearch[0].addEventListener("input", () => {
+    const filtered = components.filter(component => {
+        
+        if(component.name.toLowerCase().includes(componentSearch[0].value.toLowerCase()))
+            return component;
+    });
+    render(componentsBox, filtered); 
+});
+
+
+
 
 
